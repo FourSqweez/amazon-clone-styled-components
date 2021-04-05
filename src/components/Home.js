09560799f69b9aef2c4e56b22a263ea18c82fb98
FourSqweez <four7518@gmail.com>
2021-04-05 17:46:52 +0700

    Added : Banner background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
+import styled from 'styled-components'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container>
+      <Banner></Banner>
+
+      <Content></Content>
+    </Container>
   )
 }
 
 export default Home
+
+const Container = styled.div``
+
+const Banner = styled.div`
+  background-image: url('https://i.imgur.com/SYHeuYM.jpg');
+  min-height: 600px;
+  background-position: center;
+  background-size: cover;
+`
+
+const Content = styled.div``
