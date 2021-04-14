21624b6b72e0661bcbcda730ac9078501f4e6217
FourSqweez <four7518@gmail.com>
2021-04-14 14:29:45 +0700

    Added : NumberFormat

--- src/components/cart/CartTotal.js
+++ src/components/cart/CartTotal.js
@@ -1,14 +1,45 @@
 import styled from 'styled-components'
+import NumberFormat from 'react-number-format'
 
-const CartTotal = () => {
-  return <Container>Cart Total</Container>
+const CartTotal = ({ getCount, getTotalPrice }) => {
+  return (
+    <Container>
+      <SubTotal>
+        Subtotal ( {getCount()} items):
+        <NumberFormat
+          value={getTotalPrice()}
+          displayType={'text'}
+          thousandSeparator={true}
+          prefix={' $'}
+        />
+      </SubTotal>
+      <CheckoutButton>Proceed to checkout</CheckoutButton>
+    </Container>
+  )
 }
 
 export default CartTotal
 
 const Container = styled.div`
-  height: 200px;
   background-color: white;
   flex: 0.2;
   padding: 20px;
+`
+const SubTotal = styled.h2`
+  margin-bottom: 16px;
+`
+
+const CheckoutButton = styled.button`
+  background-color: #f0c14b;
+  width: 100%;
+  // vertical - horizontal
+  padding: 4px 8px;
+  border: 2px solid #a88734;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+
+  :hover {
+    background-color: #ddb347;
+  }
 `
